d9c9e33e78ab369f7c58b1a4fb3934a1fb6bee22
unknown <dinabeysinghe@gmail.com>
2023-07-12 11:46:11 +0530

    learne:_arry ,componets destructure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,62 @@
 import logo from './logo.svg';
 import './App.css';
 
+const courceContent = [
+  {
+    status: 'completed',
+    text: "Git (version control system) and GitHub",
+
+  },
+  {
+    status: 'completed',
+    text: "Introducion to PHP",
+
+  },
+  {
+    status: 'completed',
+    text: "HTML/CSS",
+
+  },
+  {
+    status: 'completed',
+    text: "Intoduction to Laravel",
+
+  },
+  {
+    status: 'completed',
+    text: "Laravel MVC Architecture",
+
+  },
+  {
+    status: 'completed',
+    text: "Introduction Controller",
+
+  },
+  {
+    status: 'ongoing',
+    text: "Blade engine",
+
+  },
+  {
+    status: 'ongoing',
+    text: "Model(Query builder and equelent query)",
+
+  },
+  {
+    status: 'incomplete',
+    text: "Start CRUD operations",
+
+  },
+
+];
+
 function App() {
   return (
     <div className="App">
-      <h2>React components</h2>
+      {courceContent.map((val, key) => {
+        return <MyPara key={key} status={val.status} text={val.text} />
+      })}
+      {/* <h2>React components</h2>
       <MyPara status='completed' text={'HI '} />
       <MyPara status='completed' text={'Git (version control system) and GitHub'} />
       <MyPara status='completed' text={'Basic HTML, CSS'} />
@@ -14,7 +66,7 @@ function App() {
       <MyPara status='ongoing' text={'Babel'} />
       <MyPara status='ongoing' text={'WebpacK'} />
       <MyPara status='notstart' text={'React file system'} />
-      <MyPara status='notstart' text={'package.Json'} />
+      <MyPara status='notstart' text={'package.Json'} /> */}
     </div>
   );
 }
@@ -23,8 +75,9 @@ export default App;
 
 //create new components and prints
 
-function MyPara(props) {
-  const {status,text} = props; //destructuring 
+function MyPara({status,text}) {
+  // console.log(text)
+  // const { status, text } = props; //destructuring 
   return <p style={{
     fontSize: '20px',
     color: status === 'completed' ? 'green' : status === 'ongoing' ? 'orange' : 'black'
